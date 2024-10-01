0811bd0edf58dd57ffbb3c1ba02dc28eb9bb7b2f
Julio Pedro López Bayón <juliolb@educastur.org>
2024-10-01 17:34:05 +0200

    Actualización

--- src/main/java/unidad1/expresiones/Ejercicio5.java
+++ src/main/java/unidad1/expresiones/Ejercicio5.java
@@ -1,5 +1,12 @@
 package unidad1.expresiones;
 
+/*
+ * Averigua como escribir una expresión que multiplique un número entero
+ * por una potencia de 2 sin utilizar el operador aritmético * 
+ * (multiplicación) y crea un ejemplo de prueba.
+ * 
+ */
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
