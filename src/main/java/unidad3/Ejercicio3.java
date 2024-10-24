bf7d61b4d9936b9d9beca50b4e1d83715023b1bc
Julio Pedro López Bayón <juliolb@educastur.org>
2024-10-24 21:43:40 +0200

    Actualización

---
+++ src/main/java/unidad3/Ejercicio3.java
@@ -0,0 +1,39 @@
+package unidad3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * Escribe un programa que lea dos cadenas y muestre el número de veces que la segunda
+ * está contenida en la primera.
+ * 
+ */
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Texto 1: ");
+		String s1 = in.readLine().toLowerCase();
+		System.out.println("Texto 2: ");
+		String s2 = in.readLine().toLowerCase();
+		int c = 0;
+		int i = 0;
+		
+		while((i = s1.indexOf(s2,i)) != -1) {
+			c++;
+			i++;
+		}
+		
+//		if (c == 1) {
+//			System.out.println("La cadena esta 1 vez contenida");
+//		} else {			
+//			System.out.println("La cadena esta " + c + " veces contenida");
+//		}
+		
+		System.out.println("La cadena esta " + c + " ve" + (c == 1 ? "z" : "ces") + " contenida");
+		
+	}
+
+}
