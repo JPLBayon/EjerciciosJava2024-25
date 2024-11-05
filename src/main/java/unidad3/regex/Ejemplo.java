be5d8f85b40210b45a126d68922d3f1d23f7b7e6
Julio Pedro López Bayón <juliolb@educastur.org>
2024-11-05 17:17:57 +0100

    Actualización

--- src/main/java/unidad3/regex/Ejemplo.java
+++ src/main/java/unidad3/regex/Ejemplo.java
@@ -1,5 +1,8 @@
 package unidad3.regex;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -7,12 +10,36 @@ import java.util.regex.Pattern;
 
 public class Ejemplo {
 
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
+//		try (Scanner in = new Scanner(System.in)) {
+//			System.out.print("> ");
+//			while (in.hasNext()) {
+//				Set<String> palabras = new TreeSet<>();
+//				try (Scanner s = new Scanner(Ejemplo.class.getResourceAsStream("/quijote.txt"))) {
+//
+//					String secuenciaInicial = in.nextLine();
+//					Pattern regex = Pattern.compile(secuenciaInicial + "\\w*");
+//					String palabra;
+//					while ((palabra = s.findWithinHorizon(regex, 0)) != null) {
+//						palabras.add(palabra);
+//					}
+//					System.out.println(palabras);
+//				}
+//				palabras.clear();
+//				System.out.print("> ");
+//			}
+//		}
+//
+//	}
+	
+	public static void main(String[] args) throws IOException {
+		URL url = new URL("https://www.gutenberg.org/cache/epub/2000/pg2000.txt");	
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		try (Scanner in = new Scanner(System.in)) {
 			System.out.print("> ");
 			while (in.hasNext()) {
 				Set<String> palabras = new TreeSet<>();
-				try (Scanner s = new Scanner(Ejemplo.class.getResourceAsStream("/quijote.txt"))) {
+				try (Scanner s = new Scanner(con.getInputStream())) {
 
 					String secuenciaInicial = in.nextLine();
 					Pattern regex = Pattern.compile(secuenciaInicial + "\\w*");
---
+++ src/main/java/unidad3/regex/Ejercicio2.java
@@ -0,0 +1,9 @@
+package unidad3.regex;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
