4e2bc28dbf33fd87977ca6bae2588a1995bfa5d9
Julio Pedro López Bayón <juliolb@educastur.org>
2024-12-03 17:45:19 +0100

    Actualización

---
+++ src/main/java/unidad3/arrays/Ejercicio15.java
@@ -0,0 +1,43 @@
+package unidad3.arrays;
+
+public class Ejercicio15 {
+
+	/*
+	 * Crea y retorna una matriz cuadrada de números enteros de
+	 * dimensión nxn. Antes de retornarla, la rellenará con valores
+	 * consecutivos, comenzando en 1, en el orden que se indica en el
+	 * ejemplo siguiente con una matriz cuadrada de 4x4:
+	 * 
+	 * 			1    5    9   13
+	 *          2    6   10   14
+	 *          3    7   11   15
+	 *          4    8   12   16
+	 *          		
+	 */
+	
+//	static int[][] cuadrada1(int n) {
+//		int[][] m = new int[n][n];
+//		for (int i=0; i<m.length; i++) {
+//			int v = i + 1;
+//			for (int j=0; j<m[i].length; j++) {
+//				m[i][j] = v;
+//				v += 4;
+//			}
+//		}
+//		return m;
+//	}
+	
+	static int[][] cuadrada1(int n) {
+		int[][] m = new int[n][n];
+		for (int i=0; i<n; i++) 
+			for (int j=0; j<n; j++) 
+				m[i][j] = ;
+		}
+		return m;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
