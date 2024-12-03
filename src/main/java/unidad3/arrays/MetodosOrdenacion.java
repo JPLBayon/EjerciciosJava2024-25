6516ead465e91e72c5768c1ee621d633a47b3119
Julio Pedro López Bayón <juliolb@educastur.org>
2024-12-03 16:31:18 +0100

    Actualización

--- src/main/java/unidad3/arrays/MetodosOrdenacion.java
+++ src/main/java/unidad3/arrays/MetodosOrdenacion.java
@@ -18,17 +18,6 @@ public class MetodosOrdenacion {
 		}
 	}
 	
-	static void intercambioDirecto(int [] v) {
-		for (int i=0; i < v.length; i++) {
-			for (int j = i+1; j< v.length; j++)
-				if (v[j] < v[i]) {
-					int aux = v[i];
-					v[i] = v[j];
-					v[j] = aux;
-				}
-		}
-	}
-	
 	static void seleccionDirecta(int[] v) {
 		for (int i=0; i<v.length; i++) {
 			int indice=i;
@@ -46,16 +35,28 @@ public class MetodosOrdenacion {
 		}
 	}
 	
-	
+	static void intercambioDirecto(int [] v) {
+		for (int i=0; i < v.length - 1; i++) {
+			for (int j = v.length - 1; j >i; j--)
+				if (v[j] < v[j-1]) {
+					int aux = v[j];
+					v[j] = v[j-1];
+					v[j-1] = aux;
+				}
+		}
+	}
 	public static void main(String[] args) {
 		int [] v = {44, 55, 12, 42, 94, 18, 6, 67}; 
 		System.out.println(Arrays.toString(v));
 		int [] v1 = Arrays.copyOf(v, v.length);
 		int [] v2= Arrays.copyOf(v, v.length);
+		int [] v3= Arrays.copyOf(v, v.length);
 		insercionDirecta(v1);
 		System.out.println(Arrays.toString(v1));
 		seleccionDirecta(v2);
 		System.out.println(Arrays.toString(v2));
+		intercambioDirecto(v3);
+		System.out.println(Arrays.toString(v3));
 		
 	}
 
