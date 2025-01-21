3584da4f500c8a72f4dc1f52c09a7e1a0c6be54e
Julio Pedro López Bayón <juliolb@educastur.org>
2025-01-21 16:50:10 +0100

    Actualización

--- src/main/java/unidad4/herencia/ejercicio2/PruebaPublicaciones.java
+++ src/main/java/unidad4/herencia/ejercicio2/PruebaPublicaciones.java
@@ -3,6 +3,8 @@ package unidad4.herencia.ejercicio2;
 import java.util.Arrays;
 
 import unidad4.herencia.ejercicio1.Libro;
+import unidad4.herencia.ejercicio1.Publicación;
+import unidad4.herencia.ejercicio1.Revista;
 
 public class PruebaPublicaciones {
 
@@ -10,10 +12,11 @@ public class PruebaPublicaciones {
 		Libro l1 = new Libro(3, "C", 2000, "C");
 		Libro l2 = new Libro(1, "A", 2000, "A");
 		Libro l3 = new Libro(2, "B", 2000, "B");
-		Libro [] libros = {l1, l2, l3};
-		System.out.println(Arrays.toString(libros));
-		Arrays.sort(libros);
-		System.out.println(Arrays.toString(libros));
+		Revista r1 = new Revista(4, "E", 2010, 1, 12, 3);
+		Publicación [] publicaciones = {l1, r1, l2, l3};
+		System.out.println(Arrays.toString(publicaciones));
+		Arrays.sort(publicaciones);
+		System.out.println(Arrays.toString(publicaciones));
 	}
 
 }
