232745fa0c2595a5fad9d06dd12d3027a793c220
Julio Pedro López Bayón <juliolb@educastur.org>
2025-01-30 21:27:59 +0100

    Actualización

--- src/main/java/unidad5/colecciones1/Ejercicio5.java
+++ src/main/java/unidad5/colecciones1/Ejercicio5.java
@@ -1,5 +1,33 @@
 package unidad5.colecciones1;
 
-public class Ejercicio5 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class Ejercicio5 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String linea = in.readLine();
+		int s = Integer.parseInt(linea);
+		for (int i =0; i<s; i++) {
+			linea = in.readLine();
+			String[] x = linea.split(" ");
+			int n = Integer.parseInt(x[0]);
+			int k = Integer.parseInt(x[1]);
+			ArrayList<Integer> num = new ArrayList<>();
+			linea = in.readLine();
+			x = linea.split(" ");
+			for (int it = 0; it < n; it++) {
+				num.add(Integer.parseInt(x[it]));
+
+			}
+			
+		}
+		
+		
+	}
+	
+	
 }
